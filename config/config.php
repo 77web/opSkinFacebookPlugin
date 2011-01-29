03353de5e1219b34e0de56d339eadf49f30ad577
77web <info@77-web.com>
2011-01-29 17:22:13 +0900

    initial commit

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+
+$this->dispatcher->connect('op_action.post_execute', array('opSkinFacebookPluginStyle', 'checkIfSmartPhone'));
---
+++ lib/opSkinFacebookPluginStyle.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class opSkinFacebookPluginStyle
+{
+  public static function checkIfSmartPhone(sfEvent $event)
+  {
+    $response = sfContext::getInstance()->getResponse();
+    $request = sfContext::getInstance()->getRequest();
+    
+    if($request instanceOf sfOpenPNEWebRequest && self::isSmartPhone($request->getHttpHeader('User-Agent')))
+    {
+      $response->addStylesheet('/opSkinFacebookPlugin/css/smart.css', 'last');
+      $response->addMeta('viewport', 'width=320,initial-scale=1.0,user-scalable=yes,maximum-scale=3.0');
+    }
+  }
+  
+  protected static function isSmartPhone($userAgent)
+  {
+    return self::isIphone($userAgent) || self::isAndroid($userAgent);
+  }
+  
+  protected static function isIphone($userAgent)
+  {
+    return strpos($userAgent, 'iPhone OS')!==FALSE;
+  }
+  
+  protected static function isAndroid($userAgent)
+  {
+    return strpos($userAgent, 'Linux; U; Android')!==FALSE;
+  }
+}
